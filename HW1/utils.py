679a6ef6a9d6e26c3c2ff55acbd67167fce246ba
shanjgit <lightemit@gmail.com>
2019-05-28 02:30:47 +0800

    add layers and models

--- HW1/utils.py
+++ HW1/utils.py
@@ -9,7 +9,13 @@ import tensorflow_probability as tfp
 
 class MaskedConv2D(layers.Conv2D):
     
-    def __init__(self, filters, kernel_size, mask_type='B',**kwargs):
+    def __init__(self, filters: int, kernel_size: int, mask_type='B',**kwargs):
+        '''
+        Args:
+            filters: `int`, number of output filters
+            kernel_size: `int` size of input kernel
+        
+        '''
         super(MaskedConv2D, self).__init__(filters, kernel_size, **kwargs)
         if mask_type.lower() not in ('a','b'):
             raise ValueError('mask type not in (A,B)')
@@ -26,18 +32,65 @@ class MaskedConv2D(layers.Conv2D):
 
     def build(self, input_shape):
         super(MaskedConv2D, self).build(input_shape)
-        print('Use customized mask layer')
+        #rint('Use customized mask layer')
 
         # Create a numpy array of ones in the shape of our convolution weights.
         self.mask = self.__make_mask(self.kernel_size[0], self.mask_type=='A')
         # Convert the numpy mask into a tensor mask.
         self.mask = tf.Variable(self.mask[:,:,np.newaxis, np.newaxis], trainable=False, dtype=tf.float32)
-        self.kernel = self.kernel * self.mask
-
+        
+        
+    @tf.function
     def call(self, x):
-        print()
+        self.kernel = self.kernel * self.mask
         return super(MaskedConv2D, self).call(x)
 
     def get_config(self):
         # Add the mask type property to the config.
         return dict(list(super().get_config().items()) + list({'mask': self.mask_type}.items()))
+
+    
+
+class ResNetBlock(layers.Layer):
+
+    def __init__(self, channels=128):
+        super(ResNetBlock, self).__init__()
+        self.channels = channels
+        self.conv1x1_1 = layers.Conv2D(self.channels//2, (1,1), activation='relu', padding='same')
+        self.conv1x1_2 = layers.Conv2D(self.channels, (1,1), padding='same')
+        self.conv_masked = MaskedConv2D(self.channels//2, 3, mask_type='B', activation='relu', 
+                                                                                padding='same')
+    @tf.function    
+    def call(self, inputs, debug=False):
+        if debug: tf.print('input',inputs.shape)
+        res = self.conv1x1_1(inputs)
+        if debug: tf.print('conv1.1',res.shape)
+        res = self.conv_masked(res)
+        if debug: tf.print('masked',res.shape)
+        res = self.conv1x1_2(res)
+        if debug: tf.print('conv1.2',res.shape)
+        return tf.nn.relu(inputs + res)
+
+    
+class PixelCNN(keras.Model):
+
+    def __init__(self, channels, final_channels):
+        super(PixelCNN, self).__init__()
+        self.res_blocks = [ResNetBlock(channels) for _ in range(12)]
+        self.mask_2 = MaskedConv2D(channels, 3, mask_type='B', activation='relu', padding='same')
+        self.mask_1 = MaskedConv2D(channels, 7, mask_type='A', activation='relu', padding='same')
+        self.conv1x1_1 = layers.Conv2D(channels, (1,1), activation='relu', padding='same')
+        self.conv1x1_2 = layers.Conv2D(final_channels, (1,1), padding='same')
+    
+    @tf.function
+    def call(self, inputs, training=None):
+        x = self.mask_1(inputs)
+        for res in self.res_blocks:
+            x = res(x)
+        x = self.mask_2(x)
+        x = self.conv1x1_1(x)
+        x = self.conv1x1_2(x)
+        return x
+        
+        
+         
